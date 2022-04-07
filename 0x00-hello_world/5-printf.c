760fe1acd8613af099f2b1ce7f49cd1c93503783
Bisrat24 <58773706+Bisrat24@users.noreply.github.com>
2022-04-08 00:41:48 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,6 +6,6 @@ int main() {
  * printf() displays the string inside quotation
  */
    
-   printf("with proper grammar, but the outcome is a piece of art,");
+   printf("with proper grammar, but the outcome is a piece of art,\n");
    return 0;
 }
