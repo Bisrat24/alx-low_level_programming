d7e71e5024892ced5adaae60f116cdceaf4f457e
Bisrat24 <58773706+Bisrat24@users.noreply.github.com>
2022-04-08 00:49:39 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-int main(void) 
-{
 /**
  * *main - Program starts here
  *  *Return: Success
  * printf() displays the string inside quotation
  */
+int main(void) 
+{
 puts("\"Programming is like building a multilingual puzzle");
 return 0;
 }
