35dc6849b9a6ee07443b4f574734cace6d956953
Bisrat24 <58773706+Bisrat24@users.noreply.github.com>
2022-04-08 00:44:53 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-int main() {
-   /**
+/**
  * *main - Program starts here
  *  *Return: Success
  * printf() displays the string inside quotation
  */
-   
-   printf("with proper grammar, but the outcome is a piece of art,\n");
-   return 0;
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
