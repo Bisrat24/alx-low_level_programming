b0b3e72e6ca7cc0b2f53eea32f4696a97f1e1b3d
Bisrat24 <58773706+Bisrat24@users.noreply.github.com>
2022-04-08 20:36:25 +0300

    Create 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Get a random number and print the number
+ * and if it is positive, negative, or zero
+ * Return: 0 - means success
+ */
+int main(void)
+{
+int x;
+	for (x = '0'; x <= '9'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
