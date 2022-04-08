2598784d39b544c5264811c039d594c9562fd5a8
Bisrat24 <58773706+Bisrat24@users.noreply.github.com>
2022-04-08 19:46:38 +0300

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,17 @@
-ABCDE
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Get a random number and print the number
+ * and if it is positive, negative, or zero
+ * Return: 0 - means success
+ */
+int main(void)
+{
+char x;
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
