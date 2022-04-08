d14742b102793f3981de64b4a4abc1d2041cafe9
Bisrat24 <58773706+Bisrat24@users.noreply.github.com>
2022-04-08 21:27:10 +0300

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,11 +11,11 @@ int x;
 for (x = '0'; x <= '9'; x++)
 {
 putchar(x);
-    if (x != '9')
-    {
-    putchar(',');
-    putchar(' ');
-    }
+        if (x != '9')
+        {
+        putchar(',');
+        putchar(' ');
+        }
 }
 putchar('\n');
 return (0);
