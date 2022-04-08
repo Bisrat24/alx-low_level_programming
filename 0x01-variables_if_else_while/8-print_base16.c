ef2bef6c83e05d0b3be93ab99220d62a936d34e0
Bisrat24 <58773706+Bisrat24@users.noreply.github.com>
2022-04-08 20:50:19 +0300

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,14 +9,14 @@ int main(void)
 {
 int x;
 char c;
-	for (x = '0'; x <= '9'; x++)
-	{
-		putchar(x);
-	}
-    for (c = 'a'; c <= 'f'; c++)
-	{
-		putchar(c);
-	}   
-	putchar('\n');
-	return (0);
+for (x = '0'; x <= '9'; x++)
+{
+	putchar(x);
+}
+for (c = 'a'; c <= 'f'; c++)
+{
+	putchar(c);
+}   
+putchar('\n');
+return (0);
 }
