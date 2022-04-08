fe27da14bdc673669c16008a68a66566e06e7e93
Bisrat24 <58773706+Bisrat24@users.noreply.github.com>
2022-04-08 20:43:05 +0300

    Create 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Get a random number and print the number
+ * and if it is positive, negative, or zero
+ * Return: 0 - means success
+ */
+int main(void)
+{
+char x;
+	for (x = 'z'; x >= 'a'; x--)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
