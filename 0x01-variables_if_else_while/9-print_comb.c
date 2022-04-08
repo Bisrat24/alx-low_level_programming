e99c97eedeeadb8bb058f98d5fbfd9f36c8ac1dc
Bisrat24 <58773706+Bisrat24@users.noreply.github.com>
2022-04-08 21:04:21 +0300

    Create 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Get a random number and print the number
+ * and if it is positive, negative, or zero
+ * Return: 0 - means success
+ */
+int main(void)
+{
+int x;
+	for (x = '0'; x <= '9'; x++)
+	{
+		putchar(x);
+        if (x!=9)
+        {
+        putchar(',');
+        putchar(' ');    
+        }
+	}
+	putchar('\n');
+	return (0);
+}
