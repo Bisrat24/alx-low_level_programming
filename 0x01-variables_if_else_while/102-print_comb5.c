09e4013e8d1cc3282964bad81efbaaea753e7064
Bisrat24 <58773706+Bisrat24@users.noreply.github.com>
2022-04-08 23:53:38 +0300

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,39 +7,39 @@
  */
 int main(void)
 {
-    int x;
-    int y;
-    int a;
-    int b;
-        for (x = '0'; x <= '9'; x++)
-        {
-            y = '0';
-            while (y <= '9')
-            {             
-                       for (a = '0'; a <= '9'; a++)
-                        {
-                            b = '0';
-                            while (b <= '9')
-                            {
-                                    if ((x < a) || (x == a && y < b))
-                                    {
-                                        putchar(x);
-                                        putchar(y);
-                                        putchar(' ');
-                                        putchar(a);
-                                        putchar(b);
-                                            if (!(x == '9' && y == '8'))
-                                            {
-                                                putchar(',');
-                                                putchar(' ');
-                                            }
-                                    }
-                            b++;
-                            }
-                        }        
-                y++;
-            }
-        }
-    putchar('\n');
-    return (0);
+int x;
+int y;
+int a;
+int b;
+for (x = '0'; x <= '9'; x++)
+{
+y = '0';
+while (y <= '9')
+{             
+for (a = '0'; a <= '9'; a++)
+{
+b = '0';
+while (b <= '9')
+{
+if ((x < a) || (x == a && y < b))
+{
+putchar(x);
+putchar(y);
+putchar(' ');
+putchar(a);
+putchar(b);
+if (!(x == '9' && y == '8'))
+{
+putchar(',');
+putchar(' ');
+}
+}
+b++;
+}
+}        
+y++;
+}
+}
+putchar('\n');
+return (0);
 }
