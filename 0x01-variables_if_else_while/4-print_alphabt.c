377b57d1ec5f7a43992714dcbe28f656eb72d8ea
Bisrat24 <58773706+Bisrat24@users.noreply.github.com>
2022-04-08 20:31:55 +0300

    Update 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Get a random number and print the number
+ * and if it is positive, negative, or zero
+ * Return: 0 - means success
+ */
+int main(void)
+{
+char x;
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		if (x != 'e' && x != 'q')
+		{
+			putchar(c);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
