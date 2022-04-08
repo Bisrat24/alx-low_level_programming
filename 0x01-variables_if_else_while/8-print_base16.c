31634f62a0e1051c91886614599223c6e5653fc9
Bisrat24 <58773706+Bisrat24@users.noreply.github.com>
2022-04-08 20:48:40 +0300

    Create 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Get a random number and print the number
+ * and if it is positive, negative, or zero
+ * Return: 0 - means success
+ */
+int main(void)
+{
+int x;
+char c;
+	for (x = '0'; x <= '9'; x++)
+	{
+		putchar(x);
+	}
+    for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}   
+	putchar('\n');
+	return (0);
+}
