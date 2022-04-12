62a9980b729f4e92c2eae855f294aece2d226891
Bisrat24 <58773706+Bisrat24@users.noreply.github.com>
2022-04-12 21:17:57 +0300

    Update 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+
+#include "main.h"
+
+/**
+ * _islower - check for lower case letter
+ * @c : character to check the case
+ * Return:0 or 1
+ */
+
+int _islower(int c)
+{
+if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+return (1);
+else
+return (0);
+}
