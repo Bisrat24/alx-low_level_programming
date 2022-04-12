87663b069b4f7738b6e799357c4aaf17eeba04a8
Bisrat24 <58773706+Bisrat24@users.noreply.github.com>
2022-04-12 23:04:27 +0300

    Update 11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_to_98 - print all ints to 98
+ *
+ *@n: variable to start counting from
+ *
+ *Return: none
+ */
+void print_to_98(int n)
+{
+if (n > 98)
+{
+for (; n > 98; n--)
+{
+printf("%i, ", n);
+}
+printf("98\n");
+}
+
+else if (n < 98)
+{
+for (; n < 98; n++)
+{
+printf("%i, ", n);
+}
+printf("98\n");
+}
+
+else
+printf("98\n");
+}
