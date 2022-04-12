60c4ec15d59737865ba27c458205bcd55316888a
Bisrat24 <58773706+Bisrat24@users.noreply.github.com>
2022-04-12 23:11:46 +0300

    bonus

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*print_times_table - that prints the 9 times table
+*@n: variable taken
+*Return: no return
+*/
+void print_times_table(int n)
+{
+int a, b, c;
+for (a = 0; a <= n; a++)
+{
+printf("0");
+for (b = 1; b <= n; b++)
+{
+c = a * b;
+printf(",   %d", c);
+
+}
+printf("\n");
+}
+}
