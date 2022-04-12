fd3f6050b4068222e6d8f4abf2012e2e12c80724
Bisrat24 <58773706+Bisrat24@users.noreply.github.com>
2022-04-12 21:36:07 +0300

    Update 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *jack_bauer - print every minute
+ *
+ *Return: Always 0.
+ */
+void jack_bauer(void)
+{
+int h, m, x, y, a, b;
+for (h = 0; h < 24; h++)
+{
+x = h / 10;
+y = h % 10;
+for (m = 0; m < 60; m++)
+{
+a = m / 10;
+b = m % 10;
+_putchar('0' + x);
+_putchar('0' + y);
+_putchar(':');
+_putchar('0' + a);
+_putchar('0' + b);
+_putchar('\n');
+}
+}
+}
