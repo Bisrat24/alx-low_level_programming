b2d0452c740da246e85af8a79846bd7e5e29f0a1
Bisrat24 <58773706+Bisrat24@users.noreply.github.com>
2022-04-12 21:28:34 +0300

    6&7

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_abs - Calculate absolute value of int
+ *
+ *@n: variable to check absolute value
+ *
+ *Return: Always 0.
+ */
+int _abs(int n)
+{
+if (n < 0)
+return (-n);
+else
+return (n);
+}
