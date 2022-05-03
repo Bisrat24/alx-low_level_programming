9309c662a0715096aabee0e173441fd59e7254d3
Bisrat24 <58773706+Bisrat24@users.noreply.github.com>
2022-05-03 23:11:17 +0300

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,44 +1,77 @@
-#include "main.h"
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * argstostr - concatenates all the arguments of your program.
- * @ac: the number of arguments passed including program name.
- * @av: the array of passed arguments.
+ * count_word - helper function to count the number of words in a string
+ * @s: string to evaluate
  *
- * Return: string containing all arguments.
+ * Return: number of words
  */
-char *argstostr(int ac, char **av)
+int count_word(char *s)
 {
-	char *s;
-	int i, j, k, len = 0;
+	int flag, c, w;
 
-	if (ac == 0 || av == NULL)
-		return (NULL);
+	flag = 0;
+	w = 0;
 
-	for (i = 0; i < ac; i++)
+	for (c = 0; s[c] != '\0'; c++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
-			len++;
-		len++;
-	}
-
-	s = malloc(sizeof(char) * (len + 1));
-	if (s == NULL)
-		return (NULL);
-
-	k = 0;
-
-	for (i = 0; i < ac; i++)
-	{
-		for (j = 0; av[i][j] != '\0'; j++)
+		if (s[c] == ' ')
+			flag = 0;
+		else if (flag == 0)
 		{
-			s[k] = av[i][j];
-			k++;
+			flag = 1;
+			w++;
 		}
-		s[k] = '\n';
-		k++;
 	}
 
-	return (s);
+	return (w);
+}
+/**
+ * **strtow - splits a string into words
+ * @str: string to split
+ *
+ * Return: pointer to an array of strings (Success)
+ * or NULL (Error)
+ */
+char **strtow(char *str)
+{
+	char **matrix, *tmp;
+	int i, k = 0, len = 0, words, c = 0, start, end;
+
+	while (*(str + len))
+		len++;
+	words = count_word(str);
+	if (words == 0)
+		return (NULL);
+
+	matrix = (char **) malloc(sizeof(char *) * (words + 1));
+	if (matrix == NULL)
+		return (NULL);
+
+	for (i = 0; i <= len; i++)
+	{
+		if (str[i] == ' ' || str[i] == '\0')
+		{
+			if (c)
+			{
+				end = i;
+				tmp = (char *) malloc(sizeof(char) * (c + 1));
+				if (tmp == NULL)
+					return (NULL);
+				while (start < end)
+					*tmp++ = str[start++];
+				*tmp = '\0';
+				matrix[k] = tmp - c;
+				k++;
+				c = 0;
+			}
+		}
+		else if (c++ == 0)
+			start = i;
+	}
+
+	matrix[k] = NULL;
+
+	return (matrix);
 }
