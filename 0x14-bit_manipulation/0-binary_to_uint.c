7079f35866bd5cf920f999fd1f2304869969dac2
Bisrat24 <58773706+Bisrat24@users.noreply.github.com>
2022-05-26 21:47:40 +0300

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,14 +11,11 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int ui;
 	int len, base_two;
-
 	if (!b)
 		return (0);
 
 	ui = 0;
-
 	for (len = 0; b[len] != '\0'; len++)
-		;
 
 	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
